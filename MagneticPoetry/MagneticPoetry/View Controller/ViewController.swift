a2a767266570249cc377ea7ee520e68c617e7434
Student <student@howard04.istlabs.rit.edu>
2018-02-22 14:03:23 -0500

    Trying to get the height of the word box to change

--- MagneticPoetry/MagneticPoetry/View Controller/ViewController.swift
+++ MagneticPoetry/MagneticPoetry/View Controller/ViewController.swift
@@ -22,9 +22,14 @@ class ViewController: UIViewController {
         
         placeWords(words: wordSelector.getWordSet(index: 0))
         
-        // setting word box constants
-        let screenSize: CGRect = UIScreen.main.bounds
-        WordBoxScrollView.contentSize.height = screenSize.height * 0.2
+        // set height of word box
+        WordBoxScrollView.layoutIfNeeded()
+        WordBoxScrollView.isScrollEnabled = true
+        WordBoxScrollView.contentSize.height = UIScreen.main.bounds.height * 0.25
+        print(WordBoxScrollView.contentSize.height)
+        WordBoxScrollView.contentSize = CGSize(width: self.view.frame.width, height: WordBoxScrollView.frame.size.height)
+        WordBoxScrollView.backgroundColor = UIColor.lightGray
+        
     }
     
     func placeWords(words: Array<String>) {
@@ -105,20 +110,26 @@ class ViewController: UIViewController {
     }
     
     @IBAction func ShowWordBox(_ sender: Any) {
+        
+        let wordBoxCollapseDistance: CGFloat = 0.20
+        
+        // scroll wordbox and wordSetToolbar up if collapsed
         if(isWordBoxCollapsed) {
+            
             UIView.animate(withDuration: 0.5, animations: {
-                self.WordSetToolBar.frame.origin.y -= (UIScreen.main.bounds.height * 0.2)
-                self.WordBoxScrollView.frame.origin.y -= (UIScreen.main.bounds.height * 0.2)
+                self.WordSetToolBar.frame.origin.y -= (UIScreen.main.bounds.height * wordBoxCollapseDistance)
+                self.WordBoxScrollView.frame.origin.y -= (UIScreen.main.bounds.height * wordBoxCollapseDistance)
                 
             }, completion: { (value: Bool) in
                 self.isWordBoxCollapsed = false
                 
             })
         }
+        // scroll wordbox and wordSetToolbar down if not collapsed
         else {
             UIView.animate(withDuration: 0.5, animations: {
-                self.WordSetToolBar.frame.origin.y += (UIScreen.main.bounds.height * 0.2)
-                self.WordBoxScrollView.frame.origin.y += (UIScreen.main.bounds.height * 0.2)
+                self.WordSetToolBar.frame.origin.y += (UIScreen.main.bounds.height * wordBoxCollapseDistance)
+                self.WordBoxScrollView.frame.origin.y += (UIScreen.main.bounds.height * wordBoxCollapseDistance)
                 
             }, completion: { (value: Bool) in
                 self.isWordBoxCollapsed = true
